e021547a888b2ee85e96e2fe99c2e43d68de67b2
Supawitsaelim <oatsupawit_159@outlook.com>
2023-07-05 21:32:43 +0700

    up lec2

---
+++ lecture2/Template literals.js
@@ -0,0 +1,13 @@
+console.log("\n*************** Cencatenation with template literals *******************\n")
+
+let str1 = "Supawit Saelim";
+let str2 = "Fun";
+
+console.log(`I am wriiting code in ${str1}`);
+console.log(`Formattinng in ${str1} is ${str2}`);
+
+console.log("\n*************** Expressions in template literals *******************\n")
+
+let bool = true;
+console.log(`1 + 1 is ${1 + 1}`);
+console.log(`The opposite of true is ${bool}`);
---
+++ lecture2/Undefined.js
@@ -0,0 +1,9 @@
+function test(t) {
+    if (t === undefined) {
+        return "Undefined value!";
+    }
+    return t;
+}
+
+let t;
+console.log(test(t));

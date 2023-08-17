8b0089f1ebec18ae1fe2f199ad1c88ff5d20ac5f
SupawitSaelim <109452955+SupawitSaelim@users.noreply.github.com>
2023-08-17 05:48:47 +0000

    up

--- Class_inheritance/mixin_sample.js
+++ Class_inheritance/mixin_sample.js
@@ -7,6 +7,25 @@ const MyMixin = {
 class Robot {
     constructor(model, SerialNumber ){
         this.batter = 100;
-        this.model = model
+        this.model = model;
+        this.SerialNumber = SerialNumber;
     }
 }
+
+class Human {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+Object.assign(Robot.prototype, MyMixin);
+Object.assign(Human.prototype, MyMixin);
+
+const robot = new Robot('David','KS-001');
+const person = new Human('Supawit');
+
+robot.feel();
+person.feel();
+
+
+

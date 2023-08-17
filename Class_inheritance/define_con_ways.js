3a34b861b27bbbe45291906ade0bbcadc786402f
SupawitSaelim <109452955+SupawitSaelim@users.noreply.github.com>
2023-08-17 02:11:25 +0000

    up

--- Class_inheritance/define_con_ways.js
+++ Class_inheritance/define_con_ways.js
@@ -6,7 +6,7 @@ class MyClass {
             this.age = 0
         }else if (arguments.length === 1 ){
             this.name = (typeof arguments[0] === 'string')? arguments[0] : 'N/A'
-            this.age = 0
+            this.age = 0.           
         }else if (arguments.length === 2) {
             this.name = (typeof arguments[0] === 'string') ? arguments[0] : 'N/A'
             this.age = (typeof arguments[1] === 'number')? arguments[1] : 0
